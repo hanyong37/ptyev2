6a7c22562b99129be4a8d99933f8c5f2a788b8d3
hanyong37 <chenxi0809@gmail.com>
2016-05-15 17:31:16 +0800

    add selection for customer name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,15 +1,12 @@
 class Customer < ActiveRecord::Base
   has_many :user_activities
-  default_scope {order(:created_at)}
+  default_scope {order(:name)}
 
   validates :name,presence: true
   def balance
     total_charge = self.user_activities.where(act_type: '0').sum(:amount)
 
     total_spend = self.user_activities.where(act_type: '1').sum(:amount)
-
     total_charge - total_spend
-
   end
-
 end
