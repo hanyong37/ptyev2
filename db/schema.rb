2e01c3760423fad468c44dae0142906cd66ab2e9
hanyong37 <chenxi0809@gmail.com>
2016-05-21 20:26:31 +0800

    add summary by month

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,6 @@ ActiveRecord::Schema.define(version: 20160423155430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
   create_table "catagories", force: :cascade do |t|
     t.string   "name"
