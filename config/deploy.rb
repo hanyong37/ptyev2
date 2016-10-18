814926f3fc4cc1ce5ae54f7a457cb35790a221bb
chenxi <chenxi@chenxideMacBook-Air.local>
2016-10-19 04:12:13 +0800

    add db:migrate

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-
+set :term_mode, :nil
 set :domain, 'chenxi@123.206.59.89'
 set :deploy_to, '/var/www/ptyev2'
 set :repository, 'https://github.com/hanyong37/ptyev2'
@@ -101,7 +101,7 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    # invoke :'rails:db_migrate'
+    invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
