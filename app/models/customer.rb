76393dbee95f8c01465aedd642acd8697aeebdbb
chenxi <chenxi@chenxideMacBook-Air.local>
2016-10-24 12:51:13 +0800

    add list show

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,8 @@
 class Customer < ActiveRecord::Base
   has_many :user_activities,  dependent: :restrict_with_error
- has_many :flower_orders, dependent: :restrict_with_error
+  has_many :flower_orders, dependent: :restrict_with_error
   default_scope {order('name asc')}
- #todo: 按照客户消费次数排序
+  #todo: 按照客户消费次数排序
   validates :name,presence: true
   def balance
     total_charge = self.user_activities.where(act_type: '0').sum(:amount)
